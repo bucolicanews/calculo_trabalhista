7b6e29d24f7b3b683272cd7805363830b1fe8559
teusiaqe <jmokatavares@gmail.com>
2025-10-04 21:22:40 -0300

    [dyad] Corrigida a cor do texto nos componentes Select para branco, garantindo visibilidade em fundo escuro. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/pages/WebhookConfigPage.tsx
+++ src/pages/WebhookConfigPage.tsx
@@ -167,7 +167,7 @@ const WebhookConfigPage: React.FC = () => {
                     onValueChange={(value) => handleChange(index, 'event_type', value)}
                     disabled={saving}
                   >
-                    <SelectTrigger id={`event_type-${index}`} className="bg-gray-800 border-gray-700 text-black focus:border-orange-500"> {/* Adicionado text-black aqui */}
+                    <SelectTrigger id={`event_type-${index}`} className="bg-gray-800 border-gray-700 text-white focus:border-orange-500"> {/* Corrigido para text-white */}
                       <SelectValue placeholder="Selecione um tipo de evento" />
                     </SelectTrigger>
                     <SelectContent className="bg-gray-800 border-gray-700 text-white">
